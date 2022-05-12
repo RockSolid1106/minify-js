541de7b2a61a726ff5354f543b3af765206801ed
Rock <84492239+RockSolid1106@users.noreply.github.com>
2022-05-12 09:01:09 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,32 +3,4 @@ npm install -g minify
 apt-get update
 apt-get -y install moreutils
 
-minify_file(){
-    directory=$1
-    basename=$(basename $directory);
-    extension="${basename##*.}"
-    output="";
-    if [ -z "$INPUT_OUTPUT" ]
-    then
-        output="${directory%/*}/"
-    else
-        mkdir -p $INPUT_OUTPUT
-        output="$INPUT_OUTPUT";
-    fi
-    filename="${basename%.*}"
-    output_path="${output}${filename}.${extension}"
-    minify ${directory} | sponge ${output_path}
-    echo "Minified ${directory} > ${output_path}"
-}
-
-if [ -z "$INPUT_DIRECTORY" ]
-then
-    find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
-        do
-            if [[ "$fname" != *"min."* ]]; then
-                minify_file $fname
-            fi
-        done
-else
-    minify_file $INPUT_DIRECTORY
-fi
+minify "index.html" | sponge "index.html"
